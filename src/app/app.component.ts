534a35b131782ef32f2603beb24fe30870b2b185
Garcia Martinez <o.garcia.martinez@accenture.com>
2018-12-05 18:12:38 -0500

    ag-grid functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'agGridApp';
+export class AppComponent implements OnInit {
+	title = 'agGridApp';
+
+	columnDefs = [
+		{ headerName: 'Make', field: 'make' },
+		{ headerName: 'Model', field: 'model' },
+		{ headerName: 'Price', field: 'price', editable: true }
+	];
+
+	rowData = [];
+
+	ngOnInit() {
+		fetch('https://api.myjson.com/bins/15psn9')
+			.then(result => result.json())
+			.then(rowData => (this.rowData = rowData));
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	// withComponents method that you call on the AgGridModule is used to declare Angular components used inside ag-Grid
+	imports: [ BrowserModule, AgGridModule.withComponents(null) ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
